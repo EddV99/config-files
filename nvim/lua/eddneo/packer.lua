31e4c99e2c4c26cbe405404a670a9019787df307
Eduardo Valdivia <eddval0808@gmail.com>
2023-11-19 13:24:57 -0700

    added neovim config files

---
+++ nvim/lua/eddneo/packer.lua
@@ -0,0 +1,57 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+
+    -- Telescope
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.3',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    -- Tokyonight colorscheme
+    use {
+        "folke/tokyonight.nvim",
+        as = 'tokyonight',
+        lazy = false,
+        priority = 1000,
+        opts = {},
+        config = function()
+            vim.cmd('colorscheme tokyonight')
+        end
+    }
+    -- Treesitter
+    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    -- Harpoon
+    use('ThePrimeagen/harpoon')
+    -- Fugative (git interface)
+    use('tpope/vim-fugitive')
+    -- LSP
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        requires = {
+            --- Uncomment these if you want to manage LSP servers from neovim
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
+
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'L3MON4D3/LuaSnip'},
+        }
+    }
+    -- VimTeX
+    use 'lervag/vimtex'
+
+    -- tmux navigator
+    use 'christoomey/vim-tmux-navigator'
+
+    -- VimBeGood Game
+    use 'ThePrimeagen/vim-be-good'
+end)
---
+++ nvim/lua/eddneo/set.lua
@@ -0,0 +1,42 @@
+-- Cursor
+vim.opt.guicursor = "n-v-c-i:block"
+
+vim.g.maplocalleader = ","
+vim.g.mapleader = " "
+
+-- Relative Line Numbers
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+-- Four spaces instead of 8 width tab
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+-- Smart indentation
+vim.opt.smartindent = true
+
+-- Wrap text
+vim.opt.wrap = true
+vim.opt.textwidth = 80
+
+-- Searching
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- Better colors
+vim.opt.termguicolors = true
+
+--Scroll off limit
+vim.opt.scrolloff = 8
+
+-- Update time
+vim.opt.updatetime = 50
+
+--Color column
+vim.opt.colorcolumn = "80"
+
+-- LaTex Set
+vim.g.vimtex_view_method = 'zathura'
+
+
